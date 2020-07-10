a2fcd1371ec34a678d6a891585e3ddd6a91129db
tomoaki <tomenbluecips@gmail.com>
2020-07-10 17:22:45 +0900

    first commit

---
+++ src/pages/topPage.tsx
@@ -0,0 +1,9 @@
+//  functional Component
+import React, { FC } from 'react';
+import TopHeader from '../components/topPage/topHeader';
+
+const TopPage: FC = () => {
+  return <TopHeader />;
+};
+
+export default TopPage;
